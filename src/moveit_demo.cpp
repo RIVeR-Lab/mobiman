e7f007b2a7c189ed26d9c9ff37dcad8815ea0aa2
Hongyu Li <lihongyu0807@icloud.com>
2021-12-02 02:45:30 -0500

    rework on the whole body planning, achieve higher efficiency and accuracy

--- src/moveit_demo.cpp
+++ src/moveit_demo.cpp
@@ -13,8 +13,14 @@
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 #include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 #include <visualization_msgs/Marker.h>
+#include <math.h>
+
+#define GOAL_THRESHOLD_ 0.05
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
@@ -28,9 +34,12 @@ private:
   moveit::core::RobotModelPtr kinematic_model_;
   std::vector<std::string> arm_joint_names_;
   std::vector<std::string> whole_body_joint_names_;
+  tf2_ros::Buffer buffer_;
+  tf2_ros::TransformListener tf2_;
 
 public:
-  moveit_demo(ros::NodeHandle n) : nh_(n), arm_move_group_interface_("stretch_arm"), whole_body_move_group_interface_("stretch_whole_body")
+  moveit_demo(ros::NodeHandle n) : nh_(n), arm_move_group_interface_("stretch_arm"), whole_body_move_group_interface_("stretch_whole_body"),
+                                   tf2_(buffer_)
   {
 
     // init marker publisher
@@ -74,8 +83,12 @@ public:
 
     arm_move_group_interface_.setPoseReferenceFrame("map");
     arm_move_group_interface_.setEndEffectorLink("link_grasp_center");
+    arm_move_group_interface_.setNumPlanningAttempts(10);
+    arm_move_group_interface_.setMaxVelocityScalingFactor(0.25);
+    arm_move_group_interface_.setMaxAccelerationScalingFactor(0.25);
     whole_body_move_group_interface_.setPoseReferenceFrame("map");
     whole_body_move_group_interface_.setEndEffectorLink("link_grasp_center");
+    whole_body_move_group_interface_.setNumPlanningAttempts(10);
     ROS_INFO("Pose Reference Frame is: %s", whole_body_move_group_interface_.getPoseReferenceFrame().c_str());
   };
 
@@ -148,9 +161,9 @@ public:
     marker.pose.orientation.y = 0.0;
     marker.pose.orientation.z = 0.0;
     marker.pose.orientation.w = 1.0;
-    marker.scale.x = 0.1;
-    marker.scale.y = 0.1;
-    marker.scale.z = 0.1;
+    marker.scale.x = GOAL_THRESHOLD_ * 2;
+    marker.scale.y = GOAL_THRESHOLD_ * 2;
+    marker.scale.z = GOAL_THRESHOLD_ * 2;
     marker.color.a = 1.0; // Don't forget to set the alpha!
     marker.color.r = 0.0;
     marker.color.g = 1.0;
@@ -187,38 +200,58 @@ public:
     // set the target market in rviz
     this->publish_marker(target_pose.position.x, target_pose.position.y, target_pose.position.z);
 
-    bool success;
-    success = this->whole_body_move_group_interface_.setJointValueTarget(target_pose, "link_grasp_center");
-    ROS_INFO("Whole Body IK Planner %s\n", success ? "SUCCEED" : "FAILED");
-    this->whole_body_move_group_interface_.getJointValueTarget(whole_body_joint_values);
-
-    for (std::size_t i = 0; i < this->whole_body_joint_names_.size(); ++i)
+    ros::Rate loop_rate(0.1);
+    while (true)
     {
-      ROS_INFO("#2.1 Joint %s: %f", this->whole_body_joint_names_[i].c_str(), whole_body_joint_values[i]);
+      geometry_msgs::PoseStamped gripper_pose = arm_move_group_interface_.getCurrentPose("link_grasp_center");
+      // transform to map frame
+      geometry_msgs::PoseStamped transformed_gripper_pose = buffer_.transform(gripper_pose, "map", ros::Duration(5.0));
+
+      double error = sqrt(pow(transformed_gripper_pose.pose.position.x - point.x, 2) + pow(transformed_gripper_pose.pose.position.y - point.y, 2) + pow(transformed_gripper_pose.pose.position.z - point.z, 2));
+      if (error < GOAL_THRESHOLD_)
+      {
+        ROS_INFO("Goal Pose achieved within threshold!");
+        break;
+      }
+      ROS_INFO_STREAM(transformed_gripper_pose);
+      ROS_INFO("Goal Pose not achieved! %f Replanning!", error);
+      bool success;
+      success = this->whole_body_move_group_interface_.setJointValueTarget(target_pose, "link_grasp_center");
+      ROS_INFO("Whole Body IK Planner %s\n", success ? "SUCCEED" : "FAILED");
+      this->whole_body_move_group_interface_.getJointValueTarget(whole_body_joint_values);
+
+      for (std::size_t i = 0; i < this->whole_body_joint_names_.size(); ++i)
+      {
+        ROS_INFO("#2.1 Joint %s: %f", this->whole_body_joint_names_[i].c_str(), whole_body_joint_values[i]);
+      }
+
+      double x_axis = whole_body_joint_values[0];
+      double y_axis = whole_body_joint_values[1];
+      double theta = whole_body_joint_values[2];
+
+      move_base(x_axis, y_axis, theta);
+      ROS_INFO("base finish moving");
+
+      this->arm_move_group_interface_.getJointValueTarget(arm_joint_values);
+
+      arm_joint_values[0] = whole_body_joint_values[3];
+      arm_joint_values[1] = whole_body_joint_values[4];
+      arm_joint_values[2] = whole_body_joint_values[5];
+      arm_joint_values[3] = whole_body_joint_values[6];
+      arm_joint_values[4] = whole_body_joint_values[7];
+      arm_joint_values[5] = whole_body_joint_values[8];
+
+      for (std::size_t i = 0; i < this->arm_joint_names_.size(); ++i)
+      {
+        ROS_INFO("#3.1 Joint %s: %f", this->arm_joint_names_[i].c_str(), arm_joint_values[i]);
+      }
+      success = this->arm_move_group_interface_.setJointValueTarget(arm_joint_values);
+      ROS_INFO("Arm Joint Values Set %s\n", success ? "SUCCEED" : "FAILED");
+      this->arm_move_group_interface_.move();
+      ROS_INFO("arm moving");
+
+      loop_rate.sleep();
     }
-
-    double x_axis = whole_body_joint_values[0];
-    double y_axis = whole_body_joint_values[1];
-    double theta = whole_body_joint_values[2];
-
-    this->move_base(x_axis, y_axis, theta);
-    ROS_INFO("base finish moving");
-
-    success = this->arm_move_group_interface_.setApproximateJointValueTarget(target_pose, "link_grasp_center");
-    ROS_INFO("Arm Pose Target %s\n", success ? "SUCCEED" : "FAILED");
-
-    this->arm_move_group_interface_.getJointValueTarget(arm_joint_values);
-
-    for (std::size_t i = 0; i < this->arm_joint_names_.size(); ++i)
-    {
-      ROS_INFO("#3.1 Joint %s: %f", this->arm_joint_names_[i].c_str(), arm_joint_values[i]);
-    }
-
-    this->arm_move_group_interface_.setMaxVelocityScalingFactor(0.50);
-    this->arm_move_group_interface_.setMaxAccelerationScalingFactor(0.50);
-
-    this->arm_move_group_interface_.move();
-    ROS_INFO("arm moving");
   }
 };
 
@@ -235,7 +268,7 @@ int main(int argc, char **argv)
   ROS_INFO("Listen to gripper_goal_pose");
   // listen to the input pose
   ros::Subscriber sub = node_handle.subscribe("gripper_goal_pose", 1, &moveit_demo::nav_to_goal, &demo);
-  
+
   ros::waitForShutdown();
   return 0;
 }
