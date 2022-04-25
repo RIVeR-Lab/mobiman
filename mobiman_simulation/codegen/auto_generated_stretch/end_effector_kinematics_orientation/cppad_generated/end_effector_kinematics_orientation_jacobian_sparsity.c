aa2b36cd190058eea6d6bff916ecbff054bd3209
Unver Akmandor <akmandor.n@northeastern.edu>
2022-04-25 16:36:02 -0400

    Added hsrb urdf, meshes. Added mobile manipulation launch files using ocs2 for stretch and jackal_ur5.

--- mobiman_simulation/codegen/auto_generated_stretch/end_effector_kinematics_orientation/cppad_generated/end_effector_kinematics_orientation_jacobian_sparsity.c
+++
@@ -1,9 +0,0 @@
-void end_effector_kinematics_orientation_jacobian_sparsity(unsigned long const** row,
-                                                           unsigned long const** col,
-                                                           unsigned long* nnz) {
-   static unsigned long const rows[6] = {0,0,1,1,2,2};
-   static unsigned long const cols[6] = {2,10,2,10,2,10};
-   *row = rows;
-   *col = cols;
-   *nnz = 6;
-}
--- mobiman_simulation/codegen/auto_generated_stretch/end_effector_kinematics_velocity/cppad_generated/end_effector_kinematics_velocity_jacobian_sparsity.c
+++
@@ -1,9 +0,0 @@
-void end_effector_kinematics_velocity_jacobian_sparsity(unsigned long const** row,
-                                                        unsigned long const** col,
-                                                        unsigned long* nnz) {
-   static unsigned long const rows[42] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2};
-   static unsigned long const cols[42] = {2,5,6,7,8,9,10,11,12,15,16,17,18,19,2,5,6,7,8,9,10,11,12,15,16,17,18,19,2,5,6,7,8,9,10,11,12,15,16,17,18,19};
-   *row = rows;
-   *col = cols;
-   *nnz = 42;
-}
