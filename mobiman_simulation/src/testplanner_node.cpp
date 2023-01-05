caf9231adcf03cc6fe844fe141169dd0aa995cc5
Unver Akmandor <akmandor.n@northeastern.edu>
2023-01-05 16:06:29 +0300

    Added testplanner and testplanner_node: to debug and test voxblox functions. Updated ocs2_mobile_manipulator.launch: added voxblox parameters.

---
+++ mobiman_simulation/src/testplanner_node.cpp
@@ -0,0 +1,38 @@
+#include "testplanner.h"
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "testplanner");
+
+  PlannerVoxblox planer;
+
+  while (ros::ok())
+  {
+    for (double i = -5; i < 5; i = i + 1)
+    {
+      for (double j = -10; j < 10; j = j + 1)
+      {
+        for (double k = -10; k < 10; k = k + 1)
+        {
+          Eigen::Vector3d p(i, j, k);
+          Eigen::Vector3d v(0,0,0);
+          double d;
+
+          d = planer.getMapDistanceAndGradientAtPosition(p, v);
+          
+          //std::cout << "[testplanner_node::main] d: " << d << std::endl;
+
+          if (d > 0)
+          {
+            ROS_INFO("Point = %0.2f , %0.2f , %0.2f ", p[0], p[1], p[2]);
+            ROS_INFO("Distance = %0.2f ", d);
+            ROS_INFO("Gradient = %0.7f , %0.7f , %0.7f ", v[0], v[1], v[2]);
+          }
+        }
+      }
+    }
+    ros::spinOnce();
+  }
+}
+
+
