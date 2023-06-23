0d539a0be2527f50c2cb77dda0becf780f729d65
KSorte <kaushalsorte08@gmail.com>
2023-06-23 11:36:03 -0400

    huh

--- mobiman_simulation/scripts/world_frame_broadcaster.py
+++ mobiman_simulation/scripts/world_frame_broadcaster.py
@@ -15,19 +15,21 @@ class FixedTFBroadcaster:
             rospy.sleep(0.01)
 
             t = geometry_msgs.msg.TransformStamped()
-            t.header.frame_id = "World_frame_tag"
+            t.header.frame_id = "cam2_color_optical_frame"
             t.header.stamp = rospy.Time.now()
-            t.child_frame_id = "World"
-            t.transform.translation.x = 0.0
-            t.transform.translation.y = 0.0
-            t.transform.translation.z = 0.0
+            t.child_frame_id = "world"
+            t.transform.translation.x = 0.6247646970731584
+            t.transform.translation.y = 0.5875674991736752
+            t.transform.translation.z = 2.4035590620478753
 
-            t.transform.rotation.x = 0.0
-            t.transform.rotation.y = 0.0
-            t.transform.rotation.z = 0.0
-            t.transform.rotation.w = 1.0
+            t.transform.rotation.x = 0.9283736675128444
+            t.transform.rotation.y = 0.11063793792865799
+            t.transform.rotation.z = -0.24237512714706008
+            t.transform.rotation.w = 0.2591059202335822
 
             tfm = tf2_msgs.msg.TFMessage([t])
+            print("Publishing a static world transform with respect to cam2")
+            rospy.loginfo(tfm)
             self.pub_tf.publish(tfm)
 
 if __name__ == '__main__':
