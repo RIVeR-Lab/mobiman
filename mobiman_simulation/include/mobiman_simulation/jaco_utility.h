791cd0933040c624f20256c3eb3edf0e904ba081
Sarvesh Prajapati <sarvesh101p@gmail.com>
2023-08-16 02:37:02 -0400

    update-trajectory-code

--- mobiman_simulation/include/mobiman_simulation/jaco_utility.h
+++ mobiman_simulation/include/mobiman_simulation/jaco_utility.h
@@ -2,3 +2,27 @@
 #include <kinova_msgs/JointVelocity.h>
 #include <trajectory_msgs/JointTrajectory.h>
 #include <iostream>
+#include <sensor_msgs/JointState.h>
+#include <signal.h>
+#include <vector>
+
+// Global variable current position
+sensor_msgs::JointState jaco_position;
+// Global variable target position
+
+//PID propotions
+float p = 80000.0f;
+float i = 0.0f;
+float d = 0.0f;
+float dt = 0.0f;
+float cur_time;
+float prev_time;
+std::vector<float> error(6, 0.0f);
+std::vector<float> pid(6, 0.0f);
+// Kinova_velocity msg
+kinova_msgs::JointVelocity jaco_velocity;
+
+ros::Publisher velocity_publisher;
+void position_listener(trajectory_msgs::JointTrajectory trajectory);
+void jaco_feedback(sensor_msgs::JointState joint_state);
+void shutdown_handler(int sig);
