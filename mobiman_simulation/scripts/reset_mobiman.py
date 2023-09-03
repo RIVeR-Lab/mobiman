317d078a979733a245b94d9ed12a9ebed61fae11
Sarvesh Prajapati <sarvesh101p@gmail.com>
2023-09-02 23:59:59 -0400

    added-reset-service

--- mobiman_simulation/scripts/reset_mobiman.py
+++ mobiman_simulation/scripts/reset_mobiman.py
@@ -6,25 +6,62 @@ import rosservice
 from gazebo_msgs.srv import DeleteModelRequest, DeleteModel, SpawnModel, SpawnModelRequest, SetModelConfiguration, SetModelConfigurationRequest
 from controller_manager_msgs.srv import LoadController, SwitchController, LoadControllerRequest, SwitchControllerRequest
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
-
+from mobiman_simulation.srv import resetMobiman
+import sys
+import subprocess
 
 # from std_msgs.msg import Empty as EmptyMsg
 
-if __name__ == '__main__':
-    rospy.init_node('reset_mobiman')
-    pub = rospy.Publisher('/arm_controller/command', JointTrajectory, queue_size=10)
-    joint_position = JointTrajectory()
+
+
+
+def handleResetMobiman(req):
+    # Set Pose and other variables
     pose = Pose()
     pose.position.z=0.2
     joint_names = [f'j2n6s300_joint_{str(a)}' for a in range(1,7)]
-    joint_position.joint_names = joint_names
-    # joint_position.points[0]
-    joint_position.points = [JointTrajectoryPoint(), JointTrajectoryPoint()]
     joint_positions = [0.0, 2.9, 1.3, 4.2, 1.4, 0.0]
-    joint_position.points[0].positions = joint_positions
-    joint_position.points[1].positions = joint_positions
-    joint_position.points[1].time_from_start = rospy.Duration(0.2)
     robot_model = rospy.get_param('/robot_description')
+    pause_physics_client = rospy.ServiceProxy('/gazebo/pause_physics', Empty)
+    unpause_physics_client = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
+    controller_list = ['arm_controller', 'joint_state_controller', 'jackal_velocity_controller', 'joint_group_position_controller']
+    # Delete Current Model
+    delete_model = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
+    delete_model(DeleteModelRequest('mobiman'))
+    # Spawn model
+    spawn_model = rospy.ServiceProxy('/gazebo/spawn_urdf_model', SpawnModel)
+    spawn_model(SpawnModelRequest(model_name='mobiman', model_xml=robot_model, robot_namespace='', initial_pose=pose, reference_frame='world'))
+    # Pause physics and change configuration
+    pause_physics_client(EmptyRequest())
+    set_configuration = rospy.ServiceProxy('/gazebo/set_model_configuration', SetModelConfiguration)
+    for i in range(1,100):
+        set_configuration(SetModelConfigurationRequest(model_name='mobiman', urdf_param_name='robot_description', joint_names=joint_names, joint_positions=joint_positions))
+    # Load Controllers
+    for controller in controller_list:
+        load_controller = rospy.ServiceProxy('/controller_manager/load_controller', LoadController)
+        load_controller(LoadControllerRequest(controller))
+    # Switch Controller
+    switch_controller_req = SwitchControllerRequest()
+    switch_controller_req.start_asap = True
+    for controller in controller_list:
+        command = 'sleep 0.5 && rosservice call /gazebo/unpause_physics "{}"'
+        subprocess.Popen(command, stdout=subprocess.PIPE, shell=True, stderr=subprocess.STDOUT)
+        switch_controller_req.start_controllers = [controller]
+        switch_controller = rospy.ServiceProxy('/controller_manager/switch_controller', SwitchController)
+        res = switch_controller(switch_controller_req)
+        # print(res)
+    for i in range(1, 10):
+        unpause_physics_client(EmptyRequest())
+    return
+
+
+if __name__ == '__main__':
+    rospy.init_node('reset_mobiman')
+    s = rospy.Service('reset_mobiman', resetMobiman, handleResetMobiman)
+    rospy.spin()
+    sys.exit(0)
+    
+    
     pause_physics_client = rospy.ServiceProxy('/gazebo/pause_physics', Empty)
     pause_physics_client(EmptyRequest())
     delete_model = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
@@ -33,31 +70,27 @@ if __name__ == '__main__':
     spawn_model = rospy.ServiceProxy('/gazebo/spawn_urdf_model', SpawnModel)
     spawn_model(SpawnModelRequest(model_name='mobiman', model_xml=robot_model, robot_namespace='', initial_pose=pose, reference_frame='world'))
     unpause_physics_client(EmptyRequest())
-    rospy.sleep(0.2)
+    # rospy.sleep(0.2)
     pause_physics_client(EmptyRequest())
     set_configuration = rospy.ServiceProxy('/gazebo/set_model_configuration', SetModelConfiguration)
-    set_configuration(SetModelConfigurationRequest(model_name='mobiman', urdf_param_name='robot_description', joint_names=joint_names, joint_positions=joint_positions))
+    for i in range(1,100):
+        set_configuration(SetModelConfigurationRequest(model_name='mobiman', urdf_param_name='robot_description', joint_names=joint_names, joint_positions=joint_positions))
     # rospy.sleep(0.5)
     # unpause_physics_client = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
-    unpause_physics_client(EmptyRequest())
+    
     controller_list = ['arm_controller', 'joint_state_controller', 'jackal_velocity_controller', 'joint_group_position_controller']
     for controller in controller_list:
         load_controller = rospy.ServiceProxy('/controller_manager/load_controller', LoadController)
         load_controller(LoadControllerRequest(controller))
-        switch_controller = rospy.ServiceProxy('/controller_manager/switch_controller', SwitchController)
-        res = switch_controller(SwitchControllerRequest(start_controllers=f'[{controller}]'))
-        print(res)
-    rospy.sleep(1)
+        # switch_controller = rospy.ServiceProxy('/controller_manager/switch_controller', SwitchController)
+        # res = switch_controller(SwitchControllerRequest(start_controllers=f'[{controller}]'))
+        # print(res)
+    # rospy.sleep(1)
     switch_controller_req = SwitchControllerRequest()
     switch_controller_req.start_asap = True
     for controller in controller_list:
         switch_controller_req.start_controllers = [controller]
         switch_controller = rospy.ServiceProxy('/controller_manager/switch_controller', SwitchController)
         res = switch_controller(switch_controller_req)
+        unpause_physics_client(EmptyRequest())
         print(res)
-    rate = rospy.Rate(10)
-    while pub.get_num_connections() == 0:
-        rate.sleep()
-        rospy.loginfo('wait')
-    pub.publish(joint_position)
-    # rospy.spin()
